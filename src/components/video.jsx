e505f1fab84542440855200dd8acb7c409f412d5
Clinton Templeton <donnahaytempleton1230@gmail.com>
2025-07-13 18:34:43 +0000

    adding gradient , form and video

--- src/components/video.jsx
+++ src/components/video.jsx
@@ -6,7 +6,7 @@ export default function Video() {
   return (
     <section className='h-screen w-[100%]' >
         
-        <video src={vid2} autoPlay playsInline  loop  className='h-[100%] w-[100%] border-blue-500'/>
+        <video src={vid2} autoPlay playsInline muted  loop  className='h-[100%] w-[100%] border-blue-500'/>
 
     </section>
   )
